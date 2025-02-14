2417c21b95d0c329e0b101ab168767c5d88b3620
Ignacio Gutierrez <ignachogutierrez@gmail.com>
2025-02-14 20:16:34 -0300

    feat: initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import logging
+import boto3
+from botocore.exceptions import ClientError
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+s3_bucket = os.getenv("S3_BUCKET")
+profile_name = os.getenv("AWS_PROFILE_NAME")
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    session = boto3.Session(profile_name=profile_name)
+
+    # Upload the file
+    s3_client = session.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+file_to_upload = 'doggystyle-album-cover.jpeg'
+
+upload_file(file_to_upload, s3_bucket)
